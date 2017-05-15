fd9d5bbe6bc4cc141e1014dd5a5d6b2317df59c3
Jay Carman <jwcarman@us.ibm.com>
2017-05-15 17:54:49 -0500

    feat: Enhance logdump data collection script
    Add the following command line arguments:
      -d <dir_name>           Output directoy name
      -t <tag_name>           Filename tag
      -c                      Compress result into .tgz archive
      -u                      Upload data to test server (internal only)
      -r <remote_dir>         Remote data dir (implies -u)
      -e <existing_data_dir>  Upload existing data to test server
    
    Note that the upload options ('-u', '-r', and '-e') are for development
    and test purposes only. The data repository is only accessible on an
    internal IBM network.
    
    Change-Id: I3cc9c20cf6bdea3d5c97ae0056ea9b137910cf30

--- scripts/logdump.sh
+++ scripts/logdump.sh
@@ -17,6 +17,105 @@
 
 set -e
 
+usage() {
+    echo "Cluster Genesis Log Dump Utility"
+    echo "Usage: logdump.sh [OPTION]..."
+    echo ""
+    echo "  -d <dir_name>           Output directoy name"
+    echo "  -t <tag_name>           Filename tag"
+    echo "  -c                      Compress result into .tgz archive"
+    echo "  -u                      Upload data to test server (internal only)"
+    echo "  -r <remote_dir>         Remote data dir (implies -u)"
+    echo "  -e <existing_data_dir>  Upload existing data to test server"
+    echo "  -h                      Usage help"
+}
+
+# Parse arguments
+while getopts ":d:t:cur:e:h" args; do
+    case $args in
+        d)
+            DIR=$OPTARG
+            ;;
+        t)
+            TAG="${OPTARG}."
+            ;;
+        c)
+            COMPRESS=true
+            ;;
+        r)
+            UPLOAD=true
+            UPLOAD_DIR=$OPTARG
+            ;;
+        u)
+            UPLOAD=true
+            ;;
+        e)
+            EXISTING_DIR=$OPTARG
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        ":")
+            echo "Additional argument required for option $OPTARG"
+            usage
+            exit 1
+            ;;
+        \?)
+            echo "unknown option -- $OPTARG"
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+upload_test_data ()
+{
+    echo "#######################################################"
+    echo "Uploading Data to Remote Repository..."
+
+    LOCAL_DIR=$1
+    if [ -z $2 ]; then
+        REMOTE_DIR=$(hostname)
+    else
+        REMOTE_DIR=$2
+    fi
+
+    DEPLOYER_HOSTNAME=$(hostname)
+    SSH_PRIVATE_KEY="$HOME/.ssh/id_rsa"
+    SSH_REMOTE_USER="logdumps"
+    SSH_REMOTE_HOST="9.3.210.21"
+    SSH_REMOTE="${SSH_REMOTE_USER}@${SSH_REMOTE_HOST}"
+    SSH_REMOTE_REPO="/home/${SSH_REMOTE_USER}/logdumps"
+    WEB_REMOTE_REPO="https://github.ibm.com/cluster-genesis-dev/logdumps"
+    SSH_REMOTE_SAVE="${SSH_REMOTE_REPO}/${REMOTE_DIR}"
+    BASE_REMOTE_DIR=$(basename ${LOCAL_DIR})
+    WEB_LINK="${WEB_REMOTE_REPO}/tree/master/${REMOTE_DIR}/${BASE_REMOTE_DIR}"
+    ssh-copy-id -i $SSH_PRIVATE_KEY $SSH_REMOTE
+    ssh -i $SSH_PRIVATE_KEY $SSH_REMOTE "mkdir -p ${SSH_REMOTE_SAVE}"
+    scp -i $SSH_PRIVATE_KEY -r ${LOCAL_DIR} ${SSH_REMOTE}:${SSH_REMOTE_SAVE}/.
+    ssh -i $SSH_PRIVATE_KEY $SSH_REMOTE \
+        "cd ${SSH_REMOTE_REPO}
+        git add --all
+        git commit -m 'New logdumps uploaded by ${USER}@${DEPLOYER_HOSTNAME}'
+        git push origin master"
+    echo ""
+    echo "HTTP link to data:"
+    echo "$WEB_LINK"
+    echo ""
+}
+
+# If -e flag was set upload existing folder and exit
+if [ ! -z $EXISTING_DIR ]; then
+    if [ -d "$EXISTING_DIR" ]; then
+        upload_test_data $EXISTING_DIR $UPLOAD_DIR
+        exit 0
+    else
+        echo "ERROR: $EXISTING_DIR not found!"
+        exit 1
+    fi
+fi
+
 # Set date
 DATE=$(date +%Y-%m-%dT%H%M%z)
 
@@ -42,43 +141,47 @@ MGMT_SWITCH_SSH_USER=$(awk -F: '/userid-mgmt-switch:/{print $2}' \
 DATA_SWITCH_SSH_USER=$(awk -F: '/userid-data-switch:/{print $2}' \
     ${PROJECT_DIR}/config.yml)
 
-# Create Directories
-TAG="logdump.$HOST.$DATE"
-if [ ! -d "${PROJECT_DIR}/debug_data" ]; then
-    mkdir "${PROJECT_DIR}/debug_data"
+# Create DIR is user didn't set with arg
+if [ -z $DIR ]; then
+    DIR="logdump.$HOST.$DATE"
 fi
-LOGS_DIR="${PROJECT_DIR}/debug_data/${TAG}"
+
+# Create Directories
+if [ ! -d "${PROJECT_DIR}/logdumps" ]; then
+    mkdir "${PROJECT_DIR}/logdumps"
+fi
+LOGS_DIR="${PROJECT_DIR}/logdumps/${DIR}"
 mkdir $LOGS_DIR
 
 # Local Deployer File Pointers
-DEPLOYER_INFO_SAVE="${LOGS_DIR}/${TAG}.deployer.info.txt"
+DEPLOYER_INFO_SAVE="${LOGS_DIR}/${TAG}deployer.info.txt"
 
-GIT_REPO_INFO_SAVE="${LOGS_DIR}/${TAG}.deployer.git_diff_head.txt"
+GIT_REPO_INFO_SAVE="${LOGS_DIR}/${TAG}deployer.git_diff_head.txt"
 
 CONFIG_PATH="${PROJECT_DIR}/config.yml"
-CONFIG_SAVE="${LOGS_DIR}/${TAG}.deployer.config.yml"
+CONFIG_SAVE="${LOGS_DIR}/${TAG}deployer.config.yml"
 
 CONFIG_BACKUP_PATH="${PROJECT_DIR}/config-backup.yml"
-CONFIG_BACKUP_SAVE="${LOGS_DIR}/${TAG}.deployer.config-backup.yml"
+CONFIG_BACKUP_SAVE="${LOGS_DIR}/${TAG}deployer.config-backup.yml"
 
 INV_DEPLOYER_PATH="/var/oprc/inventory.yml"
-INV_DEPLOYER_SAVE="${LOGS_DIR}/${TAG}.deployer.inventory.yml"
+INV_DEPLOYER_SAVE="${LOGS_DIR}/${TAG}deployer.inventory.yml"
 
 ANSIBLE_LOG_PATH="${PLAYBOOKS}/ansible.log"
-ANSIBLE_LOG_SAVE="${LOGS_DIR}/${TAG}.deployer.ansible.log"
+ANSIBLE_LOG_SAVE="${LOGS_DIR}/${TAG}deployer.ansible.log"
 
 LXC_CONF_PATH="${PLAYBOOKS}/lxc.conf"
-LXC_CONF_SAVE="${LOGS_DIR}/${TAG}.deployer.lxc.conf"
+LXC_CONF_SAVE="${LOGS_DIR}/${TAG}deployer.lxc.conf"
 
-MGMT_SWITCH_SAVE="${LOGS_DIR}/${TAG}.mgmt_switch"
-DATA_SWITCH_SAVE="${LOGS_DIR}/${TAG}.data_switch"
+MGMT_SWITCH_SAVE="${LOGS_DIR}/${TAG}mgmt_switch"
+DATA_SWITCH_SAVE="${LOGS_DIR}/${TAG}data_switch"
 
 # Container File Pointers
 INV_CONTAINER_PATH="~/cluster-genesis/inventory.yml"
-INV_CONTAINER_SAVE="${LOGS_DIR}/${TAG}.container.inventory.yml"
+INV_CONTAINER_SAVE="${LOGS_DIR}/${TAG}container.inventory.yml"
 
 GENESIS_LOG_PATH="~/cluster-genesis/log.txt"
-GENESIS_LOG_SAVE="${LOGS_DIR}/${TAG}.container.log.txt"
+GENESIS_LOG_SAVE="${LOGS_DIR}/${TAG}container.log.txt"
 
 # Functions to save files
 save_local_file ()
@@ -250,28 +353,46 @@ save_container_file $GENESIS_LOG_PATH $GENESIS_LOG_SAVE
 get_config_key "ipaddr-mgmt-switch"
 for ip in "${config_value[@]}"; do
     echo "Collecting mgmt switch config from ${MGMT_SWITCH_SSH_USER}@$ip"
-    ssh ${MGMT_SWITCH_SSH_USER}@$ip 'en; conf t; line vty length 0; sh run' \
-        > "$MGMT_SWITCH_SAVE.$ip.log" || \
-        echo "WARNING: ssh returned non-zero return code!"
+    if ping -c 1 $ip; then
+        ssh ${MGMT_SWITCH_SSH_USER}@$ip \
+            'en; conf t; line vty length 0; sh run' \
+            > "$MGMT_SWITCH_SAVE.$ip.log" || \
+            echo "WARNING: ssh returned non-zero return code!"
+    else
+        echo "WARNING: Switch $ip does not ping!"
+    fi
 done
 
 # Save data switch(es) configuration
 get_config_key "ipaddr-data-switch"
 for ip in "${config_value[@]}"; do
     echo "Collecting data switch config from ${DATA_SWITCH_SSH_USER}@$ip"
-    ssh ${DATA_SWITCH_SSH_USER}@$ip \
-        'cli enable show\ running-config show\ vlan show\ mac-address-table' \
-        > "$DATA_SWITCH_SAVE.$ip.log" || \
-        echo "WARNING: ssh returned non-zero return code!"
+    if ping -c 1 $ip; then
+        ssh ${DATA_SWITCH_SSH_USER}@$ip \
+            'cli en show\ running-config show\ vlan show\ mac-address-table' \
+            > "$DATA_SWITCH_SAVE.$ip.log" || \
+            echo "WARNING: ssh returned non-zero return code!"
+    else
+        echo "WARNING: Switch $ip does not ping!"
+    fi
 done
 
 # Tar & compress logs
-tar -cvzf "${LOGS_DIR}.tgz" -C "${PROJECT_DIR}/debug_data" "${TAG}"
+if [ ! -z $COMPRESS ]; then
+    tar -cvzf "${LOGS_DIR}.tgz" -C "${PROJECT_DIR}/logdumps" "${DIR}"
+fi
 
 # Print Archive Location
 echo
 echo "#######################################################"
 echo "Script Completed! Data has been collected and saved to:"
 echo "${LOGS_DIR}"
-echo "${LOGS_DIR}.tgz"
+if [ ! -z $COMPRESS ]; then
+    echo "${LOGS_DIR}.tgz"
+fi
 echo
+
+# Upload data to test server if -u argument was passed
+if [ ! -z $UPLOAD ]; then
+    upload_test_data $LOGS_DIR $UPLOAD_DIR
+fi
