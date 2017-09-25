db1871fa88a068d2d1cd25ed81ff9af29b3dfab2
Raymond J. Harrington <rharrington@us.ibm.com>
2017-09-25 10:18:27 -0500

    feat: Add bmc reset capability to tear-down
    Add new bmc_reset.py script which displays a list of responding systems
    from the inventory file in /var/oprc and then asks if the user wants
    to reset the BMC for those systems.
    
    Reorder tear-down to destoy container before tear down of bridges and
    include the ability to reset BMCs after destroying container.
    
    Add show_power_status.py which can be called from the command line
    to show the IPMI chassis power status of all systems in /var/oprc/
    inventory.py.
    
    Include pyghmi in scripts/install.sh in the list of packages installed
    in the virtualenv.
    
    Change-Id: I87eba9b112224c1b971f0ad1fbacc4f8fce7c74b

---
+++ scripts/python/show_power_status.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# Copyright 2017 IBM Corp.
+#
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import nested_scopes, generators, division, absolute_import, \
+    with_statement, print_function, unicode_literals
+
+import sys
+import readline
+from pyghmi.ipmi import command as ipmi_command
+from pyghmi import exceptions as pyghmi_exception
+
+from lib.inventory import Inventory
+from lib.logger import Logger
+
+
+def rlinput(prompt, prefill=''):
+    readline.set_startup_hook(lambda: readline.insert_text(prefill))
+    try:
+        return raw_input(prompt)
+    finally:
+        readline.set_startup_hook()
+
+
+def Power_Status(log, inv_file):
+    inv = Inventory(log, inv_file)
+
+    print('Systems in Cluster Genesis inventory: ')
+
+    systems = []
+
+    for rack, ipv4, user, passwd in inv.yield_ipmi_access_info():
+        ipmi_cmd = ipmi_command.Command(
+            bmc=ipv4,
+            userid=user,
+            password=passwd)
+        try:
+            status = ipmi_cmd.get_power()
+            systems.append({'ipv4': ipv4, 'user': user, 'password': passwd})
+        except pyghmi_exception.IpmiException as error:
+            print(
+                'BMC Power status failed - Rack: %s - IP: %s, %s' %
+                (rack, ipv4, str(error)))
+
+        print('Rack: {} - IP: {} Power status: {}'.format(rack, ipv4, status))
+
+
+if __name__ == '__main__':
+    """
+    Arg1: inventory file
+    Arg2: log level
+    """
+    LOG = Logger(__file__)
+
+    if len(sys.argv) != 3:
+        try:
+            raise Exception()
+        except:
+            sys.exit('Invalid argument count')
+
+    INV_FILE = sys.argv[1]
+    LOG.set_level(sys.argv[2])
+    Power_Status(LOG, INV_FILE)
