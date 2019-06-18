b51612075e68765b8558a6765e6974f5b9659c35
Jay W. Carman <jwcarman@us.ibm.com>
2019-06-18 10:08:59 -0500

    feat: Add new 'osinstall' fields

--- scripts/python/osinstall.py
+++ scripts/python/osinstall.py
@@ -165,14 +165,10 @@ def render_kickstart(profile_object, kickstart_template=None):
         if kickstart_out.endswith('.j2'):
             kickstart_out = kickstart_out[:-3]
 
-        j2_vars = {'default_user': 'rhel75',
-                   'default_pass': 'passw0rd',
-                   'pass_crypted': False,
+        j2_vars = {'pass_crypted': False,
                    'install_disk': None,
-                   'domain': 'localdomain',
                    'timezone': 'America/Chicago',
-                   'utc': True,
-                   }
+                   'utc': True}
 
         pxe_network = IPNetwork(p_netw.pxe_subnet_cidr)
         j2_vars['http_server'] = ip_route_get_to(str(pxe_network.ip))
@@ -182,6 +178,12 @@ def render_kickstart(profile_object, kickstart_template=None):
                                                 j2_vars['http_repo_name'])
 
         j2_vars['hostname'] = p_node.hostname
+
+        j2_vars['domain'] = p_node.domain
+
+        j2_vars['default_user'] = p_node.username
+
+        j2_vars['default_pass'] = p_node.password
 
         with open(kickstart_template, 'r') as file_object:
             template = Template(file_object.read())
